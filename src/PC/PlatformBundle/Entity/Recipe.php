50e13fe68b37caa4822a20f465d27a98927aeb0d
fromdanut <remidelannoy@hotmail.com>
2017-09-07 23:45:23 +0200

    Add hover effect on small recipe

--- src/PC/PlatformBundle/Entity/Recipe.php
+++ src/PC/PlatformBundle/Entity/Recipe.php
@@ -219,7 +219,7 @@ class Recipe
      * @return string
      */
     public function getShortDescription()
-    {
+    {   
         return $this->shortDescription;
     }
 
--- src/PC/PlatformBundle/Resources/public/js/recipe_index.js
+++ src/PC/PlatformBundle/Resources/public/js/recipe_index.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
+    // Add the chosen-select on left menu.
     $('select[class*=".chosen"]').chosen({
         no_results_text: "Oups, aucune catégorie trouvée pour : ",
         placeholder_text_multiple: "catégorie",
     });
+
+    // Change color on hover on small recipe.
+    $('div[class="row SmallRecipeContainer"]').mouseenter(function() {
+        $(this).css('background-color', 'rgb(255,165,0)');
+    })
+    $('div[class="row SmallRecipeContainer"]').mouseleave(function() {
+        $(this).css('background-color', '#485563');
+    })
+
 });
